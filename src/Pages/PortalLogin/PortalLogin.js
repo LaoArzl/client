94f935e7d9e9b43b9f9abb6684e95ff823c7a3ff
Arzl James <arzljames15@gmail.com>
2021-03-19 02:55:17 +0800

    Update PortalLogin.js

--- src/Pages/PortalLogin/PortalLogin.js
+++ src/Pages/PortalLogin/PortalLogin.js
@@ -83,7 +83,7 @@ const PortalLogin = () => {
             }}
           >
             <div className="portal-login-label">
-              <label>Username</label>
+              <label>Usernames</label>
             </div>
             <div className="portal-login-input">
               <input
